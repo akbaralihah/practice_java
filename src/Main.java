ed2bba2b3062188b91d66655c144bf4fa5736d63
ez4python <akbaralisalohiddinov808@gmail.com>
2025-08-25 10:15:02 +0500

    user register

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String[] fullName = scanner.nextLine().split(" ");
+        System.out.printf("Firstname: %s\nLastname: %s", fullName[0], fullName[1]);
+
+        scanner.close();
     }
 }
